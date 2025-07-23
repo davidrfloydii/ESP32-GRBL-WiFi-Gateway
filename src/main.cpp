97e723fea034cea9d1c9bf24e1bdffc20437df0c
davidrfloydii <david.r.floyd.ii@gmail.com>
2025-07-23 15:00:18 -0500

    Added some comments.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
+/* Simple pass through that allows clients like Universal GCode Sender to 
+connect, then messages received will be passed through to the GRBL controller via the 
+ESP32 serial port. */
 #include <Arduino.h>
 #include "secrets.h"
 #include <WiFi.h>
-
+// Setup your WIFI credentials in the secrets.h file. See README.MD for more details.
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PWD;
-
+// Define both the server and the client.
 WiFiServer server(23);
 WiFiClient client;
-
+// Two variables for commands one incoming and one outgoing.
 String incomingLine = "";
 String commandFromTelnet = "";
-
+// Setup method connects to WiFi, and starts the telnet server.
 void setup() {
   Serial.begin(115200);
   Serial1.begin(115200, SERIAL_8N1, 16, 17); // UART to GRBL
